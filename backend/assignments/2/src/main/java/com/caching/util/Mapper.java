163775efad8f9b5331acb349574d21c5784befe0
Sasi Rachapotu <sasivenkatakiran.rachapotu@kickdrumtech,com>
2024-01-27 20:48:45 +0530

    Mapper fixed

--- backend/assignments/2/src/main/java/com/caching/util/Mapper.java
+++ backend/assignments/2/src/main/java/com/caching/util/Mapper.java
@@ -24,7 +24,6 @@ public class Mapper {
                 return null;
             }
         } catch (JsonProcessingException e) {
-            e.printStackTrace();
             return null;
         }
     }
